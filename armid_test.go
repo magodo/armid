9ed860f93894a7859994bc27c2c629c2c025aac6
magodo <wztdyl@sina.com>
2022-09-15 11:08:09 +0800

    Revert "`Parent()` returns its `ParentScope()` if this is a root scoped resource"
    This reverts commit 3b8155425b089ab3022d075b591d61028acdaa29.

--- armid_test.go
+++ armid_test.go
@@ -240,7 +240,7 @@ func TestResourceId_Parent(t *testing.T) {
 				AttrTypes:       []string{"foos"},
 				AttrNames:       []string{"foo1"},
 			},
-			expect: &TenantId{},
+			expect: nil,
 		},
 		{
 			name: "Child Scoped Resource under tenant",
